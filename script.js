e669930bfecd580f5507543f40170b81f1b684b5
Mostafa Eltohamy <mostafa.eltohamy@gmail.com>
2022-12-22 02:41:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const hideAnswer = document.querySelector(".answer");
 const showAnswer = document.querySelector(".arrow" );
+const question = document.querySelector('h3');
 
 hideAnswer.style.display= 'none';
 showAnswer.addEventListener('click', function(){
@@ -8,6 +9,7 @@ showAnswer.addEventListener('click', function(){
     showAnswer.style.transform = 'rotate(180deg)';
     if(hideAnswer.style.display === 'none'){
         hideAnswer.style.display = 'block';
+        
     }
     else{
     hideAnswer.style.display = 'none'
@@ -17,79 +19,79 @@ showAnswer.addEventListener('click', function(){
     }
 });
 
-// // ---------------------------------------------------------------------2
+// ---------------------------------------------------------------------2
 
-// const hideAnswerB = document.querySelector("#answer2");
-// const showAnswerB = document.querySelector("#arrow2" );
+const hideAnswerB = document.querySelector("#answer2");
+const showAnswerB = document.querySelector("#arrow2" );
 
-// hideAnswerB.style.display= 'none';
-// showAnswerB.addEventListener('click', function(){
+hideAnswerB.style.display= 'none';
+showAnswerB.addEventListener('click', function(){
 
-//     showAnswerB.style.transform = 'rotate(180deg)';
-//     if(hideAnswerB.style.display === 'none'){
-//         hideAnswerB.style.display = 'block';
-//     }
-//     else{
-//     hideAnswerB.style.display = 'none'
-//     showAnswerB.style.transform = 'none';
-
- 
-//     }
-// });
-// // ------------------------------------------------------------3
-// const hideAnswer3 = document.querySelector("#answer3");
-// const showAnswer3 = document.querySelector("#arrow3" );
-
-// hideAnswer3.style.display= 'none';
-// showAnswer3.addEventListener('click', function(){
-
-//     showAnswer3.style.transform = 'rotate(180deg)';
-//     if(hideAnswer3.style.display === 'none'){
-//         hideAnswer3.style.display = 'block';
-//     }
-//     else{
-//     hideAnswer3.style.display = 'none'
-//     showAnswer3.style.transform = 'none';
+    showAnswerB.style.transform = 'rotate(180deg)';
+    if(hideAnswerB.style.display === 'none'){
+        hideAnswerB.style.display = 'block';
+    }
+    else{
+    hideAnswerB.style.display = 'none'
+    showAnswerB.style.transform = 'none';
 
  
-//     }
-// });
-// // -----------------------------------------------------------------4
-// const hideAnswer4 = document.querySelector("#answer4");
-// const showAnswer4 = document.querySelector("#arrow4" );
+    }
+});
+// ------------------------------------------------------------3
+const hideAnswer3 = document.querySelector("#answer3");
+const showAnswer3 = document.querySelector("#arrow3" );
 
-// hideAnswer4.style.display= 'none';
-// showAnswer4.addEventListener('click', function(){
+hideAnswer3.style.display= 'none';
+showAnswer3.addEventListener('click', function(){
 
-//     showAnswer4.style.transform = 'rotate(180deg)';
-//     if(hideAnswer4.style.display === 'none'){
-//         hideAnswer4.style.display = 'block';
-//     }
-//     else{
-//     hideAnswer4.style.display = 'none'
-//     showAnswer4.style.transform = 'none';
-
- 
-//     }
-// });
-// // ------------------------------------------------------------------5
-// const hideAnswer5 = document.querySelector("#answer5");
-// const showAnswer5 = document.querySelector("#arrow5" );
-
-// hideAnswer5.style.display= 'none';
-// showAnswer5.addEventListener('click', function(){
-
-//     showAnswer5.style.transform = 'rotate(180deg)';
-//     if(hideAnswer5.style.display === 'none'){
-//         hideAnswer5.style.display = 'block';
-//     }
-//     else{
-//     hideAnswer5.style.display = 'none'
-//     showAnswer5.style.transform = 'none';
+    showAnswer3.style.transform = 'rotate(180deg)';
+    if(hideAnswer3.style.display === 'none'){
+        hideAnswer3.style.display = 'block';
+    }
+    else{
+    hideAnswer3.style.display = 'none'
+    showAnswer3.style.transform = 'none';
 
  
-//     }
-// });
+    }
+});
+// -----------------------------------------------------------------4
+const hideAnswer4 = document.querySelector("#answer4");
+const showAnswer4 = document.querySelector("#arrow4" );
+
+hideAnswer4.style.display= 'none';
+showAnswer4.addEventListener('click', function(){
+
+    showAnswer4.style.transform = 'rotate(180deg)';
+    if(hideAnswer4.style.display === 'none'){
+        hideAnswer4.style.display = 'block';
+    }
+    else{
+    hideAnswer4.style.display = 'none'
+    showAnswer4.style.transform = 'none';
+
+ 
+    }
+});
+// ------------------------------------------------------------------5
+const hideAnswer5 = document.querySelector("#answer5");
+const showAnswer5 = document.querySelector("#arrow5" );
+
+hideAnswer5.style.display= 'none';
+showAnswer5.addEventListener('click', function(){
+
+    showAnswer5.style.transform = 'rotate(180deg)';
+    if(hideAnswer5.style.display === 'none'){
+        hideAnswer5.style.display = 'block';
+    }
+    else{
+    hideAnswer5.style.display = 'none'
+    showAnswer5.style.transform = 'none';
+
+ 
+    }
+});
 
 
 
